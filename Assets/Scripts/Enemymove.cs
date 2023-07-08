d268183ba226d4d3591f8e4d137da0fe77483ec8
SatoshiFurutani <chips.potato.tomato@gmail.com>
2023-07-08 18:57:42 +0900

    Enemy修正

--- Assets/Scripts/Enemymove.cs
+++ Assets/Scripts/Enemymove.cs
@@ -6,13 +6,10 @@ public class Enemymove : MonoBehaviour
 {
     [SerializeField] private float enemySpeed;
     [SerializeField] private float enemyLifeTime;
-    private GameObject data;
-    private Data dataCs;
     // Start is called before the first frame update
     void Start()
     {
-        data = GameObject.Find("Data");
-        dataCs = data.GetComponent<Data>();
+        
     }
 
     // Update is called once per frame
@@ -23,14 +20,10 @@ public class Enemymove : MonoBehaviour
         enemyLifeTime = enemyLifeTime - Time.deltaTime;
         if(enemyLifeTime < 0)
         {
-            Destory(this.gameObject);
+            Destroy(this.gameObject);
         }
 
     }
 
-    private void OnTrigerEnter(Collider other)
-    {
-        dataCs.score++;
-        Destory(this.gameObject);
-    }
+ 
 }
